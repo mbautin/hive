762e702d55495176e942ab830f31f00328ad2e8e
Mikhail Bautin <mbautin@gmail.com>
2013-06-21 16:32:20 -0700

    Add a git log of most recent Hive comments and print hive_version in prepare_to_test.sh

--- bin/build/prepare_to_test.sh
+++ bin/build/prepare_to_test.sh
@@ -3,6 +3,15 @@
 set -e -u -o pipefail
 cd $(dirname $0)/../..
 
+echo "Building Hive on $HOSTNAME" >&2
+if [ -n "$hive_version" ]; then
+  echo "Using Hive version ${hive_version}"
+fi
+echo
+echo "Most recent Hive commits:"
+git log -n 10
+echo
+
 echo "--------------------------------------------------------------------------------"
 echo "build.properties"
 echo "--------------------------------------------------------------------------------"
