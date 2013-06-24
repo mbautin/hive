30c51fa5c007b02eeb3de697fbe225b5f3f37744
Mikhail Bautin <mbautin@gmail.com>
2013-06-23 18:29:46 -0700

    Remove hive-builtins from Maven and Ivy caches before the build

--- bin/build/prepare_to_test.sh
+++ bin/build/prepare_to_test.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Used when running Hive tests on Jenkins.
+
 set -e -u -o pipefail
 cd $(dirname $0)/../..
 
 echo "Building Hive on $HOSTNAME" >&2
-if [ -n "$hive_version" ]; then
+if [ -n "${hive_version:-}" ]; then
   echo "Using Hive version ${hive_version}"
+else
+  echo "Error: hive_version is not set" >&2
+  exit 1
 fi
 echo
 echo "Most recent Hive commits:"
@@ -21,3 +26,15 @@ cat build.properties
 echo
 echo "--------------------------------------------------------------------------------"
 echo
+
+for d in ~/.m2 ~/.ivy2/cache; do
+  if [ -d "${d}" ]; then
+    set -x
+    find "${d}" \( \
+      -name "hive-builtins-${hive_version}.jar" -or \
+      -wholename "*/${hive_version}/jars/hive_builtins.jar" \
+    \) -exec bash -c "set -x; rm -f {}" \;
+    set +x
+  fi
+done
+
